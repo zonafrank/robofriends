3e8348f968b3ccd0dc74b9b70d19348a4f6ffef6
Eche Oji <eche@debian01.virtual-network.com>
2020-06-14 15:39:27 +0100

    Updates

---
+++ precache-manifest.63bd200e1f39732d97904192b3640ac8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c8a1e65d6878f350f83f1f8c49e611",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6048105db420adc56bf9",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2a5dc92c457a6cf89e71",
+    "url": "/robofriends/static/css/main.64184a27.chunk.css"
+  },
+  {
+    "revision": "6048105db420adc56bf9",
+    "url": "/robofriends/static/js/2.dd0088b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.dd0088b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5dc92c457a6cf89e71",
+    "url": "/robofriends/static/js/main.473d1bc0.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
